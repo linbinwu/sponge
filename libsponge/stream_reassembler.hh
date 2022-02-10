34bd01ef7518336a9b4b030d1dd64ec12987ccae
linbinwu <linbinwu1999@gmail.com>
2022-02-10 19:41:35 +0800

    merge lab1 code

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -4,6 +4,7 @@
 #include "byte_stream.hh"
 
 #include <cstdint>
+#include <vector>
 #include <string>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
@@ -14,6 +15,11 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+
+    std::vector<char> _unassembled_bytes;
+	std::vector<bool> _unassembled_exist;
+    size_t _unassembled_bytes_count;
+    uint64_t _eof_index;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
